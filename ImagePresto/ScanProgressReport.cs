f798273b7ed35a158bbffac835b2f4c15de3da84
Gernot Lenkner <g.lenkner@outlook.de>
2020-12-06 09:59:18 +0100

    + task scan report

---
+++ ImagePresto/ScanProgressReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagePresto
+{
+    class ScanProgressReport
+    {
+        //current progress
+        public int CurrentProgressAmount { get; set; }
+
+        //total progress
+        public int TotalProgressAmount { get; set; }
+
+        //some message to pass to the UI of current progress
+        public string CurrentProgressMessage { get; set; }
+    }
+}
