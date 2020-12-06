d65e55ea9ab809e0912af04a2ed02232c743191c
Gernot Lenkner <g.lenkner@outlook.de>
2020-12-06 08:31:56 +0100

    Added Config

---
+++ ImagePresto/IPConfig.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagePresto
+{
+    class IPConfig
+    {
+        public string FileList = default;
+
+        public static IPConfig GetConfig()
+        {
+            return JsonConvert.DeserializeObject<IPConfig>(File.ReadAllText("ipconfig.json"));
+        }
+
+        internal void Save()
+        {
+            string res = JsonConvert.SerializeObject(this);
+        }
+    }
+}
