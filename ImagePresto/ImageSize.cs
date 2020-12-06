2b46f1940bcd29fca6127c2134a546be3ec657b4
elfen20 <elfen20@outlook.de>
2020-12-06 11:33:35 +0100

    + image size record

---
+++ ImagePresto/ImageSize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagePresto
+{
+    struct ImageSize
+    {
+        public string fName;
+        public Size size;
+    }
+}
