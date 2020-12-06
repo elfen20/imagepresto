0cc8574535bdc04bab9fac409ccc480b12418aa7
Gernot Lenkner <g.lenkner@outlook.de>
2020-12-06 08:32:15 +0100

    added scan form

---
+++ ImagePresto/ImageScan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImagePresto
+{
+    public partial class ImageScan : Form
+    {
+        readonly string ScanDir;
+        readonly IPConfig config;
+        public ImageScan(string v)
+        {
+            InitializeComponent();
+            this.ScanDir = v;
+            config = IPConfig.GetConfig();
+        }
+    }
+}
